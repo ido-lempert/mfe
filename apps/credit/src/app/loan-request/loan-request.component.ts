b1b8f40f234a0954a033b4ec19330379a3d4ed37
Ido Lempert <ido.lempert@poalim.co.il>
2021-11-03 18:18:10 +0200

    missing files

---
+++ apps/credit/src/app/loan-request/loan-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'poalim-loan-request',
+  templateUrl: './loan-request.component.html',
+  styleUrls: ['./loan-request.component.scss']
+})
+export class LoanRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
